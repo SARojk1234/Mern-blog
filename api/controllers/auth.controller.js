1c8d285b7312ac1519555b9508f35cb55db5300c
Saroj Pandey <104881681+SARojk1234@users.noreply.github.com>
2024-01-25 10:51:04 +0530

    create or add a middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,13 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup = async(req,res)=>{
+import { errorHandler } from '../utilis/error.js';
+
+
+export const signup = async(req,res,next)=>{
     const { username, email, password} = req.body;
 
     if(!username || !email || !password || username ==='' || email ==='' || password ===''){
-        return res.status(400).json({message:'All fields are required'});
+        next(errorHandler(400, 'all fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -19,7 +22,7 @@ export const signup = async(req,res)=>{
         res.json('SignUp Successful');
         
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
         
     }
    
