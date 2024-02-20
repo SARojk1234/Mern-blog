2bc7515b26aa602afc72e439184cad262aae0be3
SARojk1234 <sarojking720@gmail.com>
2024-02-20 11:27:16 +0530

    add show functionality to dashboard posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table } from "flowbite-react";
+import {  Table } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  const [showMore, setShowMore] = useState(true)
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -14,6 +15,7 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length<9) setShowMore(false);
         }
       } catch (error) {
         console.log(error.message);
@@ -23,6 +25,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async()=>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+      
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
 
   return (
     <>
@@ -81,6 +101,11 @@ export default function DashPosts() {
                 </Table.Body>
               ))}
             </Table>
+            {
+              showMore && (
+                <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show more</button>
+              )
+            }
           </>
         ) : (
           <p>You have no posts yet!</p>
